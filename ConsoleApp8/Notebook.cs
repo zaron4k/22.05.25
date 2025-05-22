9f15613bdb1a4d70c54b575aaeb16ac51d95bba5
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-05-22 10:55:35 +0300

    Add files via upload

---
+++ ConsoleApp8/Notebook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Notebook
+    {
+        public string model;
+        public string proizv;
+        public int price;
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook temp = new Notebook();
+            temp.model = "Ultra";
+            temp.proizv = "Noname";
+            temp.price = 20000;
+            Console.WriteLine($"Модель:{temp.model}\tПроизводитель:{temp.proizv}\tЦена:{temp.price}");
+            Console.Read();
+        }
+    }
+}
