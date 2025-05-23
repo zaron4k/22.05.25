5379cc41fa1b525763dfd647c33820722dc6b100
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-05-23 12:48:00 +0300

    Add files via upload

---
+++ ConsoleApp11/MyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    /// <summary>
+    /// Класс открытого поля change
+    /// </summary>
+    class MyClass
+       
+    {
+        public string change;    
+    }
+    /// <summary>
+    /// Структура с публичным полем change
+    /// </summary>
+    struct MyStruct
+    {
+        public string change;
+    }
+
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //класс
+            MyClass myClass = new MyClass();
+            //структура
+            MyStruct myStruct = new MyStruct();
+            myClass.change = "не изм.";
+            myStruct.change = "не изм.";
+            Console.WriteLine("До вызова метода");
+            Console.WriteLine($"MyClass.change={myClass.change}");
+            Console.WriteLine($"MyStruct.change={myStruct.change}");
+            //вызываем методы
+            ClassTaker(myStruct);
+            StructTaker(myStruct);
+            Console.Read();
+        }
+        /// <summary>
+        /// в этом методе пояснение что метод принимает объект класса и изменяет значение поле change
+        /// </summary>
+        /// <param name="myClass">myClass</param>
+        static void ClassTaker(MyClass myClass)
+        {
+
+            myClass.change = "изм."; 
+        }
+        static void StructTaker (MyStruct myStruct)
+        {
+            myStruct.change = "изм.";
+        }
+    }
+}
