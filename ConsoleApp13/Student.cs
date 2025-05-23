9e0d2812408d2da337b37121e54f1f152955c451
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-05-23 12:48:38 +0300

    Add files via upload

---
+++ ConsoleApp13/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+/// <summary>
+/// описать структуру с полями ФИО, группа, информатика, физика, история. создать массив или колекцию из n объектов данной структуры.
+///ввод данных осуществлять с клавиатуры сведение об объектах вывести в табличном видею в случае отсутствие информании об объект вывести информацию об осутсвие объекта.
+ ///определить средний бал оценок по предмету. вывести студентов общий бал который выше 4 и подсчитать их колличество.
+/// </summary>
+class Student
+{
+    struct Students
+    {
+        public string FIO;
+        public string Group;
+        public int Informatics;
+        public int Physics;
+        public int History;
+        public int SRB;
+    }
+    public void info(string FIO, string Group, int Informatics, int Physics, int History, int SRB)
+    {
+        Console.WriteLine($"ФИО:{FIO}");
+        Console.WriteLine($"Оценка по физики:{Physics}");
+        Console.WriteLine($"Оценка по истории:{History}");
+        Console.WriteLine($"Оценка по информатике:{Informatics}");
+        Console.WriteLine($"Средний балл:{SRB}");
+    }
+   
+}
